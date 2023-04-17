6156bec9ee71ff9e10faef7396c85561fc32253d
ReinisTams <125902838+ReinisTams@users.noreply.github.com>
2023-04-17 12:12:25 +0300

    Update hash_substring.py

--- hash_substring.py
+++ hash_substring.py
@@ -1,33 +1,33 @@
-
 #python3-2
 
-def read_input():
+    def read_input():
     input_type = input().rstrip()
     if input_type == "i":
+
         p = input().rstrip()
         t = input().rstrip()
         return (p, t)
+
     elif input_type == "f":
         f = input()
         test ='./tests/'
         file = test+f
         with open(file, 'r') as f:
-            return (f.readline().rstrip(),f.readline().rstrip())
+        return (f.readline().rstrip(),f.readline().rstrip())
 
-    
-
-def print_occurrences(output):
+    def print_occurrences(output):
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
-    
+    def get_occurrences(pattern, text):
+ 
     q = 156
     w = 256
+
     p_len = len(pattern)
     p_hash = 0
     t_len = len(text)
     t_hash = 0
+
     h = 1
     result = []
     for i in range(p_len-1):
@@ -45,8 +45,8 @@ def get_occurrences(pattern, text):
                     match = False
                     break
 
-            if match:
-                result.append(i)
+        if match:
+            result.append(i)
 
         if i < t_len - p_len:
             t_hash = (w * (t_hash - ord(text[i]) * h) + ord(text[i + p_len])) % q
@@ -55,8 +55,5 @@ def get_occurrences(pattern, text):
                 t_hash += q
 
     return result
-
-
-# this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
